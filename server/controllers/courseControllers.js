a352dd22406e1d9bb1b91e46f7e3251825044c86
Souvik Sen <88578056+Souvik0901@users.noreply.github.com>
2024-01-24 20:38:17 +0530

    Update courseControllers.js

--- server/controllers/courseControllers.js
+++ server/controllers/courseControllers.js
@@ -3,14 +3,14 @@ const Courses = require('../models/courses');
 
 
 
+
 const createCourseWithImage = async (req, res) => {
+ 
+
   try {
-
     if (req.file) {
+      const imageUrl = `/node/api/core/images/${req.file.filename}`;
 
-      const imageUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/node/api/core/images/${req.file.filename}`;
-      
-      // Continue with course creation
       const course = new Courses({
         courseImage: imageUrl,
         courseTitle: req.body.courseTitle,
@@ -24,6 +24,7 @@ const createCourseWithImage = async (req, res) => {
         period: req.body.period,
         purchaseDate: req.body.purchaseDate,
         videoLink: req.body.videoLink,
+        user_id: req.user.userId,
       });
 
       await course.save();
@@ -39,38 +40,84 @@ const createCourseWithImage = async (req, res) => {
 
 
 
-
 // get all courses
 const getCourses = async (req, res) => {
-  const courses = await Courses.find().sort({ createdAt: -1 });
+
+  const { userId } = req.user;
+  const courses = await Courses.find({ user_id: userId }).sort({ createdAt: -1 });
+  if (!courses) {
+    return res.status(401).json({ message: 'Customer does not exists' });
+  }
   res.status(200).json(courses);
 };
-
-
 
 // delete a single course
 const deleteCourse = async (req, res) => {
   const { id } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).json({ error: 'No such imagedata' });
+    return res.status(400).json({ error: 'No such courseCollections' });
   }
 
   const course = await Courses.findOneAndDelete({ _id: id });
 
   if (!course) {
-    return res.status(404).json({ error: 'no such imagedata' });
+    return res.status(404).json({ error: 'no such courseCollections' });
   }
   return res.status(200).json(course);
 };
 
+const updateCourse = async (req, res) => {
+  try {
+    const courseId = req.params.id;
 
+    if (!courseId) {
+      return res.status(400).json({ success: 0, message: 'Invalid course ID' });
+    }
 
+    // Find the existing course by ID
+    const existingCourse = await Courses.findById(courseId);
 
+    if (!existingCourse) {
+      return res.status(404).json({ success: 0, message: 'Course not found' });
+    }
+
+    // Update the course fields based on the request body
+    existingCourse.courseTitle = req.body.courseTitle || existingCourse.courseTitle;
+    existingCourse.shortDescrp = req.body.shortDescrp || existingCourse.shortDescrp;
+    existingCourse.longDescrp = req.body.longDescrp || existingCourse.longDescrp;
+    existingCourse.courseCategory = req.body.courseCategory || existingCourse.courseCategory;
+    existingCourse.courseLevel = req.body.courseLevel || existingCourse.courseLevel;
+    existingCourse.courseLanguage = req.body.courseLanguage || existingCourse.courseLanguage;
+    existingCourse.lectures = req.body.lectures || existingCourse.lectures;
+    existingCourse.price = req.body.price || existingCourse.price;
+    existingCourse.period = req.body.period || existingCourse.period;
+    existingCourse.purchaseDate = req.body.purchaseDate || existingCourse.purchaseDate;
+    existingCourse.videoLink = req.body.videoLink || existingCourse.videoLink;
+
+    // Check if an image is being uploaded
+    if (req.file) {
+      existingCourse.courseImage = `/node/api/core/images/${req.file.filename}`;
+    }
+
+    // Save the updated course
+    await existingCourse.save();
+
+    res
+      .status(200)
+      .json({ success: 1, message: 'Course update successful', course: existingCourse });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ success: 0, message: 'Internal server error' });
+  }
+
+  return null;
+};
 
 module.exports = {
   getCourses,
   deleteCourse,
-
+  updateCourse,
   createCourseWithImage,
 };
+
