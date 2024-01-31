0d5d62245e6313d0d6b84c63daec84632bf68395
Souvik Sen <88578056+Souvik0901@users.noreply.github.com>
2024-01-31 12:53:05 +0530

    responseObject Added

--- server/routes/router.js
+++ server/routes/router.js
@@ -2,11 +2,14 @@
 import express from 'express';
 import multer from 'multer';
 
-/* Importing the functions from the auth and health controller files. */
+/* Importing the functions from  controller files. */
 import checkConnection from '../controllers/checkConnection';
 import userController from '../controllers/userControllers';
 import courseControllers from '../controllers/courseControllers';
 import getUserControllers from '../controllers/getUserControllers';
+import editProfile from '../controllers/editProfile';
+
+/* Importing the functions from middleware files. */
 import authenticateUser from '../middleware/authenticateUser';
 
 // file upload or image upload using multer fuction
@@ -31,6 +34,13 @@ router.post('/login', userController.login);
 router.post('/signup', userController.signup);
 router.post('/logout', userController.logout);
 router.get('/getuserdata', authenticateUser.verifytoken, getUserControllers.getUser);
+router.post('/changepassword', authenticateUser.verifytoken, editProfile.changePassword);
+router.post(
+  '/updateprofileinfo',
+  authenticateUser.verifytoken,
+  upload.single('profileImg'),
+  editProfile.changeUserdata,
+);
 
 router.post(
   '/createcoursewithimage',
@@ -41,7 +51,12 @@ router.post(
 router.get('/getcourses', authenticateUser.verifytoken, courseControllers.getCourses);
 router.get('/paginatedcourses', authenticateUser.verifytoken, courseControllers.paginatedCourses);
 router.delete('/deletecourse/:id', authenticateUser.verifytoken, courseControllers.deleteCourse);
-router.patch('/updatecourse/:id', upload.single('courseImage'), courseControllers.updateCourse);
+router.patch(
+  '/updatecourse/:id',
+  authenticateUser.verifytoken,
+  upload.single('courseImage'),
+  courseControllers.updateCourse,
+);
 
 /* Exporting the router object. */
 export default router;
