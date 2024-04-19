0a3512ab22008a42be17b35234ae4f12d263841f
Souvik Sen <88578056+Souvik0901@users.noreply.github.com>
2024-04-19 10:59:16 +0530

    Create Popup.js

---
+++ client/CreateCourse/Popup.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+
+const Popup = (props) => {
+  return (props.trigger)?(
+    <div className='popup'>
+      
+     
+
+            <div className="modal-dialog">
+                <div className="modal-content">
+                      <div className="modal-header bg-dark">
+                        <h5 className="modal-title text-white" id="addLectureLabel">Add Lecture</h5>
+                        <button type="button" className="btn btn-sm btn-light mb-0" data-bs-dismiss="modal" aria-label="Close" onClick={()=>props.setTrigger(false)}><i className="bi bi-x-lg"></i></button>
+                      </div>
+                      <div className="modal-body">
+                        <form className="row text-start g-3">
+                          
+                          <div className="col-12">
+                            <label className="form-label">Course name <span className="text-danger">*</span></label>
+                            <input type="text" className="form-control" placeholder="Enter course name"/>
+                          </div>
+                        </form>
+                      </div>
+                      <div className="modal-footer">
+                        <button type="button" className="btn btn-danger-soft my-0" data-bs-dismiss="modal" onClick={()=>props.setTrigger(false)}>Close</button>
+                        <button type="button" className="btn btn-success my-0">Save Lecture</button>
+                      </div>
+                </div>
+            </div>
+      
+
+    </div>
+
+  ):"";
+}
+
+export default Popup
